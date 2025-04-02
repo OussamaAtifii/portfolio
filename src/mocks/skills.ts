2f62dab6464b1245c94aa9c14e73531c217c4876
OussamaAtifii <oussamaati03@gmail.com>
2025-04-02 12:42:58 +0200

    chore: add skills mock

---
+++ src/mocks/skills.ts
@@ -0,0 +1,20 @@
+import type { Skill } from "@customTypes/skill";
+
+export const skills: Skill[] = [
+    {
+      name: "Languages",
+      stack: ["JavaScript", "TypesScript", "Java", "PHP", "SQL"]
+    },
+    {
+      name: "Frontend",
+      stack: ["Angular", "React", "Astro", "Tailwind", "HTML", "CSS"]
+    },
+    {
+      name: "Backend",
+      stack: ["Spring Boot", "Node.js", "Express", "Laravel"]
+    },
+    {
+      name: "Others",
+      stack: ["Git", "Github", "Linux", "Docker", "RESTful APIs"]
+    }
+  ]
