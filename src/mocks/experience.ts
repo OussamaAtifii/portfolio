77836df286f2c4427732fb94171fe1d82e62341e
OussamaAtifii <oussamaati03@gmail.com>
2025-04-03 23:10:27 +0200

    feat: add mock experience data

---
+++ src/mocks/experience.ts
@@ -0,0 +1,16 @@
+import type { Experience } from "@customTypes/experience"
+
+export const experience: Experience[] = [
+  {
+    title: "FullStack Developer",
+    company: "Magical Analytics",
+    companyLink: "https://magical.es/",
+    date: "March 2024 - Present",
+    description: [
+      "Developed Laravel-based services to transfer large data volumes from ERP systems to HubSpot, integrating HubSpot API to centralize and sync information.",
+      "Built custom REST APIs and HubSpot interfaces to visualize real-time entity relationships, using Laravel, JavaScript, and Tailwind CSS",
+      "Created and implemented automated HubSpot workflows using Node.js to simplify processes and improve efficiency.",
+      "Created dynamic, responsive WordPress websites to enhance clients' digital presence.",
+    ],
+  },
+]
