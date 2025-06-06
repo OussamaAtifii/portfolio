8776a311326044d872bf10b5ee0c1212407c96ae
OussamaAtifii <oussamaati03@gmail.com>
2025-06-06 23:59:57 +0200

    feat: update skills mock data with new categories and technologies

--- src/mocks/skills.ts
+++ src/mocks/skills.ts
@@ -1,20 +1,24 @@
 import type { Skill } from "@customTypes/skill";
 
 export const skills: Skill[] = [
-    {
-      name: "Languages",
-      stack: ["JavaScript", "TypesScript", "Java", "PHP", "SQL"]
-    },
-    {
-      name: "Frontend",
-      stack: ["Angular", "React", "Astro", "Tailwind", "HTML", "CSS"]
-    },
-    {
-      name: "Backend",
-      stack: ["Spring Boot", "Node.js", "Express", "Laravel"]
-    },
-    {
-      name: "Others",
-      stack: ["Git", "Github", "Linux", "Docker", "RESTful APIs"]
-    }
-  ]
+  {
+    name: "Frontend",
+    stack: ["React", "Angular", "Tailwind", "HTML", "CSS"],
+  },
+  {
+    name: "Backend",
+    stack: ["Laravel", "Spring Boot", "Node.js", "NestJS"],
+  },
+  {
+    name: "Lenguajes",
+    stack: ["JavaScript", "TypeScript", "PHP", "Java", "SQL"],
+  },
+  {
+    name: "DevOps",
+    stack: ["Docker", "AWS", "Vercel", "Linux"],
+  },
+  {
+    name: "Herramientas",
+    stack: ["Git", "VS Code", "IntelliJ", "Postman", "Figma"],
+  },
+];
